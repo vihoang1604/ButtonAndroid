7f3804f876227c77274496e52b06fc8111bd5206
Hoang Thanh Vi <vi.hoang1@pn.com.vn>
2019-09-11 11:06:50 +0700

    Final Button

--- app/src/main/java/com/example/button/MainActivity.java
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.button_id);
+         button.setText("Summit2");
+         button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Toast.makeText(MainActivity.this,"Click",Toast.LENGTH_SHORT).show();
+             }
+         });
     }
 }
